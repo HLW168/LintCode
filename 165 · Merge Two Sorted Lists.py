178d05802d95cc8fe34d9beef09d39c96c3cf7fd
HLW168 <88788406+HLW168@users.noreply.github.com>
2021-08-11 10:36:03 -0500

    Create 165 · Merge Two Sorted Lists.py

---
+++ 165 · Merge Two Sorted Lists.py
@@ -0,0 +1,34 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param l1: ListNode l1 is the head of the linked list
+    @param l2: ListNode l2 is the head of the linked list
+    @return: ListNode head of linked list
+    """
+    def mergeTwoLists(self, l1, l2):
+        # write your code here
+        dummy = ListNode(None)
+        head = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = l1
+                l1 = l1.next
+            else:
+                head.next = l2
+                l2 = l2.next
+            head = head.next
+                
+        if l1:
+            head.next = l1
+        if l2:
+            head.next = l2
+        
+        return dummy.next
