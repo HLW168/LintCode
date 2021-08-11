c4b9996feb62deb099c51096466c70088fae5c35
HLW168 <88788406+HLW168@users.noreply.github.com>
2021-08-11 10:28:54 -0500

    Create 452 · Remove Linked List Elements.py

---
+++ 452 · Remove Linked List Elements.py
@@ -0,0 +1,24 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @param val: An integer
+    @return: a ListNode
+    """
+    def removeElements(self, head, val):
+        # write your code here
+        dummy = ListNode(-1, head)
+        p = dummy
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return dummy.next
